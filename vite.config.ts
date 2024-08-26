6f02d603800c2199b277a079ff81cd45cc37573f
Kareem Adel <kareemadel10110@gmail.com>
2024-08-27 02:19:34 +0300

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
  
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/rock-paper-scissors/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
